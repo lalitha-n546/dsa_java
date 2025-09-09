1d8d5b5de18add69fe516ef5c8bea80291aeb435
lalitha-n546 <lalitha12m143@gmail.com>
2025-09-09 20:36:19 +0530

    First program

--- dsa-practice/StringPalindrome.java
+++ dsa-practice/StringPalindrome.java
@@ -1,16 +1,14 @@
-// Question:Check whether the given user string is palindrome or not.
+
 import java.util.*;
 
 public class StringPalindrome {
-    
-    static String ispalindrome(String word) {
+    static String palindrome(String word) {
         if (word == null || word.isEmpty()) {
             return "Invalid input. Please enter a valid word.";
         }
 
         int n = word.length();
         int c = 0;
-
 
         for (int i = 0; i < n; i++) {
             if (word.charAt(i) == word.charAt(n - i - 1)) {
@@ -29,7 +27,8 @@ public class StringPalindrome {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word of your choice: ");
         String word = sc.nextLine();
-        System.out.println(ispalindrome(word));
+        System.out.println(palindrome(word));
         sc.close();
     }
 }
+
